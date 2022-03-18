a2f3eab26c88da872ad7e14db797c2315f8aec5f
CarlosHernandez0101 <82908248+CarlosHernandez0101@users.noreply.github.com>
2022-03-18 07:33:27 -0600

    Create Posts View

---
+++ ZemogaMobileTest/View/Screens/PostsView.swift
@@ -0,0 +1,20 @@
+//
+//  PostsView.swift
+//  ZemogaMobileTest
+//
+//  Created by Carlos Hernández on 18/03/22.
+//
+
+import SwiftUI
+
+struct PostsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostsView()
+    }
+}
