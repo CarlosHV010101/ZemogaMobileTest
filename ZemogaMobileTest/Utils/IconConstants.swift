fda0ed685f4adb7f8813e4ef4d13dd2852e23995
CarlosHernandez0101 <82908248+CarlosHernandez0101@users.noreply.github.com>
2022-03-18 07:38:16 -0600

    Create IconConstants

---
+++ ZemogaMobileTest/Utils/IconConstants.swift
@@ -0,0 +1,18 @@
+//
+//  IconConstants.swift
+//  ZemogaMobileTest
+//
+//  Created by Carlos Hernández on 18/03/22.
+//
+
+import Foundation
+
+public enum IconConstants {
+    
+    enum ToolBarIcons {
+        static let back = ""
+        static let reload = ""
+        static let favorite = ""
+    }
+    
+}
