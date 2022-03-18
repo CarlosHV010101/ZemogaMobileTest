655fcedf29a32ef124ba7dcfe396d5acfd3ae78c
CarlosHernandez0101 <82908248+CarlosHernandez0101@users.noreply.github.com>
2022-03-18 07:36:18 -0600

    Create NavigationBarView

---
+++ ZemogaMobileTest/View/Shared/NavigationBar/NavigationBarView.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationBarView.swift
+//  ZemogaMobileTest
+//
+//  Created by Carlos Hernández on 18/03/22.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        HStack {
+            
+        }
+        .background(Color.green)
+    }
+}
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+    }
+}
